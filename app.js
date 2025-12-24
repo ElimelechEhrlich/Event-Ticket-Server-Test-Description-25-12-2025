f17e3edd2ab9c67d89d086f49cd68e1c72b82df6
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-24 17:02:33 +0200

    create routers and Preparing the routes

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ import notesRouter from "./routes/notesR.js"
 import { jsonBodyGuard, requestLogger } from "./middlewares/middlewares.js"
 
 const app = express()
+const port = 3000
 
 app.use(express.json(), requestLogger, jsonBodyGuard)
 
 app.use("/contacts", contactsRouter)
 app.use("/notes", notesRouter)
+
+app.listen(port, () => {
+    console.log(`server runing on http://localhost:${port}`);
+})
 
 
 
