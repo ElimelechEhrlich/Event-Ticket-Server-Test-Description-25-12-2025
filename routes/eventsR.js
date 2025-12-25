c79f85316d680eaf1ee3d6eaa3baba6d03784ca5
Elimelech Ehrlich <ee525566@gmail.com>
2025-12-25 09:12:12 +0200

    prepare files and folders

---
+++ routes/eventsR.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const router = express.Router()
+
+router.get("/:noteId", async (req, res) => {})
+router.delete("/:noteId", async (req, res) => {})
+
+export default router
+
